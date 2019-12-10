71c6c41eea58951531abe255925d5f0fc469e643
Rizik <rizikardi01@gmail.com>
2019-12-10 10:19:52 +0700

    Belajar Operator di Java

--- CekGanjilGenap.java
+++ CekGanjilGenap.java
@@ -1,9 +1,10 @@
 public class CekGanjilGenap {
 		public static void main (String[] args){
 			
-			int nilai = Integer.parseInt(args[0]);
-			String cek = nilai%2 == 0 ? "Genap":"Ganjil";
-			System.out.println(cek);
+			int input = Integer.parseInt(args[0]);
+			String hasil = input %2 == 0 ? "Genap":"Ganjil";
+			System.out.println("Program Test Angka Ganjil Genap");
+			System.out.println(input + " Ini adalah bilangan " + hasil);
 			
 		}
 }
